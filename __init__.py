92e3b4cd90eb25fad5bf4be6f462194dce79b1f6
Senofu <senofu@foxmail.com>
2024-04-27 16:10:08 +0800

    修改description

--- __init__.py
+++ __init__.py
@@ -1,21 +1,23 @@
-from nonebot import get_plugin_config,logger,get_driver
-from nonebot.plugin import PluginMetadata,on_message,on
-from nonebot.adapters.onebot.v11 import Bot,Event,MessageSegment
+from nonebot import get_plugin_config, logger, get_driver
+from nonebot.plugin import PluginMetadata, on_message, on
+from nonebot.adapters.onebot.v11 import Bot, Event, MessageSegment
 from nonebot.rule import to_me
 from nonebot.matcher import Matcher
 from .config import Config
 
 __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_ddbotatall",
-    description="为DDBOT-WS适配的简易@全体插件",
-    usage="",
+    description="为DDBOT-WS适配的简易@全体插件，在@全体次数不够的情况下@SUPERUSER",
+    usage="None",
     config=Config,
+    supported_adapters={"~onebot.v11"},
 )
 
 config = get_plugin_config(Config)
 
-atall = on_message(rule=lambda event: event.user_id == event.self_id, priority=1)
-on_message_sent = on("message_sent", block = False)
+atall = on_message(rule=lambda event: event.user_id ==
+                   event.self_id, priority=1)
+on_message_sent = on("message_sent", block=False)
 
 config = get_driver().config
 superusers = list(config.superusers)
@@ -35,6 +37,6 @@ async def atall_handler(bot: Bot, matcher: Matcher, event: Event):
     if tmp in text:
         try:
             logger.success(f"在群聊{group_id}中 尝试@全体")
-            await bot.send(event, message = atall_segment)
+            await bot.send(event, message=atall_segment)
         except Exception as e:
-            await bot.send(event, message = "@全体次数不足或出错 "+atadmin_segment)
+            await bot.send(event, message="@全体次数不足或出错 "+atadmin_segment)
